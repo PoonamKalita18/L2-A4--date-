82f5b9bb34ef4da959da3882df93d0d40fde8142
PoonamKalita18 <arunakalita2015@gmail.com>
2024-10-15 21:43:14 +0530

    time

---
+++ main.py
@@ -0,0 +1,9 @@
+import datetime
+
+current_time = datetime.datetime.now()
+print("time at greenwich meridian is", end="")
+print(current_time)
+
+import calendar
+
+print("\n", calendar.calendar(2008))
